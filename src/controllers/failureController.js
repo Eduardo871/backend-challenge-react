ed93269257426bdbd84dc04d946bc40b362af786
sebastian <eduardociare871@gmail.com>
2022-01-24 17:08:32 -0300

    fixed backend

--- src/controllers/failureController.js
+++ src/controllers/failureController.js
@@ -1,5 +1,5 @@
 const failure  = (req,res) =>{
-    res.staus(400).send({message: "The password or email is incorrect", content:[]})
+    res.status(400).send({message: "The password or email is incorrect", content:[]})
 }
 
 module.exports = {
