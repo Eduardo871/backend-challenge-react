3adb4f2f38666187ef3229c52383a3d0b47c18a6
sebastian <eduardociare871@gmail.com>
2022-01-24 15:34:41 -0300

    api fixed errors

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,7 +12,7 @@ server.get('/failureRedirect', failure);
 server.post('/login',
 passport.authenticate('local',{
     successRedirect:'/',
-    failureRedirect: '/failureRedirect'
+    failureRedirect: '/auth/failureRedirect'
 }),
 (req, res, next) => {req.logIn(); next()}
 
